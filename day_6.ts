3c3da3e63fcc831bdf633514cd3ecab1554e452e
christophe jeunesse <christophe.jeunesse@orange.com>
2022-12-06 07:08:13 +0100

    [Day 6] Ts version

--- day_6.ts
+++ day_6.ts
@@ -1,20 +1,27 @@
 import * as Utils from "./utils";
 import { Part, run, Type } from "./day_utils"
+import { FrequencyMap } from "./frequencyMap";
 
 function parse(lines: string[]): string[] {
-    return lines;
+    return lines[0].split("");
 }
 
 function solve(lines: string[], part: Part): void {
     const data = parse(lines);
-    if (part === Part.PART_1) {
-        const result = data.length;
-        console.log(`Result ${result}`)
+    const nbDistinct = part === Part.PART_1 ? 4 : 14;
+    const frequencyMap = new FrequencyMap<string>();
+    for (let pos = 0; pos < data.length; pos++) {
+        if (frequencyMap.getTotalItemsCount() === nbDistinct) {
+            frequencyMap.remove(data[pos - nbDistinct]);
+        }
+        frequencyMap.add(data[pos]);
+        if (frequencyMap.getDistinctItemsCount() === nbDistinct) {
+            console.log(`Result ${pos+1}`);
+            break;
+        }
     }
-    else {
-        const result = data.length;
-        console.log(`Result ${result}`);
-    }
+
+
 }
 
 run(6, [Type.TEST, Type.RUN], solve, [Part.PART_1, Part.PART_2]);
